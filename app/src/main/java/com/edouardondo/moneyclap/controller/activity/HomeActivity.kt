21db1d2394b7421b2e9054d9b2a95d3357c20aa6
Axel ESSONO <naxelcdric@gmail>
2021-01-24 00:31:04 +0100

    Create HomeActivity

---
+++ app/src/main/java/com/edouardondo/moneyclap/controller/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.edouardondo.moneyclap.controller.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.edouardondo.moneyclap.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
