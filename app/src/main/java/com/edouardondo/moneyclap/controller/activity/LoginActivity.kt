bec1ec05d9205fbd037479262859540e45a3d18b
Axel ESSONO <naxelcdric@gmail>
2021-01-24 18:03:49 +0100

    Create login function and pass data to HomeActivity

--- app/src/main/java/com/edouardondo/moneyclap/controller/activity/LoginActivity.kt
+++ app/src/main/java/com/edouardondo/moneyclap/controller/activity/LoginActivity.kt
@@ -1,14 +1,30 @@
 package com.edouardondo.moneyclap.controller.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.edouardondo.moneyclap.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_login)
+
+        login()
+    }
+
+    fun login() {
+        val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+        intent.putExtra("Email", email_edt.text.toString().trim())
+        intent.putExtra("Password", password_edt.text.toString().trim())
+        startActivity(intent)
+        finish()
     }
 }
