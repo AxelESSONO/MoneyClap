ea3d3ef4df141a8cfa5a846c122a68ad7b3561b5
Axel ESSONO <naxelcdric@gmail>
2021-01-28 14:49:27 +0100

    Added these 4 fragments to view devis feature

---
+++ app/src/main/java/com/edouardondo/moneyclap/controller/fragment/subFragment/devis/AllDevisFragment.kt
@@ -0,0 +1,21 @@
+package com.edouardondo.moneyclap.controller.fragment.subFragment.devis
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.edouardondo.moneyclap.R
+
+class AllDevisFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_devis, container, false)
+    }
+
+}
