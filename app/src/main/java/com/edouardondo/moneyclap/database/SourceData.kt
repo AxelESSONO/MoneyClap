25645304798f2d6624658c4da43bf88e1c077759
Axel ESSONO <naxelcdric@gmail>
2021-01-28 14:29:49 +0100

    Added list of transaction feature and devis feature

--- app/src/main/java/com/edouardondo/moneyclap/database/SourceData.kt
+++ app/src/main/java/com/edouardondo/moneyclap/database/SourceData.kt
@@ -12,18 +12,19 @@ abstract class SourceData
         val featureList = mutableListOf<Feature>()
 
         val devisFeatureList = mutableListOf<DevisFeature>(
-            DevisFeature(R.drawable.ic_devis_edit,"Faire un devis"),
-            DevisFeature(R.drawable.ic_find_devis,"Rechercher un devis"),
-            DevisFeature(R.drawable.ic_modify_devis,"Modifier un devis"),
-            DevisFeature(R.drawable.ic_all_devis,"Voir tous les devis"))
+            DevisFeature(R.drawable.ic_devis_edit,"Faire un devis".toUpperCase()),
+            DevisFeature(R.drawable.ic_find_devis,"Rechercher un devis".toUpperCase()),
+            DevisFeature(R.drawable.ic_modify_devis,"Modifier un devis".toUpperCase()),
+            DevisFeature(R.drawable.ic_all_devis,"Voir tous les devis".toUpperCase()),
+        )
 
         val transactionList = mutableListOf<Transaction>(
-            Transaction("Toutes les transactions"),
-            Transaction("Transactions en cours"),
-            Transaction("Transactions effectuées"),
-            Transaction("Transactions annulées"), //
-            Transaction("Modifier une transaction"),
-            Transaction("Annuler une transaction")
+            Transaction(1,"Toutes les transactions", R.drawable.ic_all_transfert),
+            Transaction(2,"Valider une transaction", R.drawable.ic_validate_transfert),
+            Transaction(3,"Transactions en cours", R.drawable.ic_ongoing),
+            Transaction(4,"Transactions effectuées", R.drawable.ic_done),
+            Transaction(5,"Transactions annulées", R.drawable.ic_baseline_cancel_24), //
+            Transaction(6,"Annuler une transaction", R.drawable.ic_cancel_transaction),
         )
     }
 }
