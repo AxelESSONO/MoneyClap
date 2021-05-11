2abbb7bd5c8d3e98b9d19906d0881c909518939e
Axel ESSONO <naxelcdric@gmail>
2021-05-12 00:12:49 +0200

    Created RateActivity.kt to manage rate

---
+++ app/src/main/java/com/edouardondo/moneyklap/controller/activity/RateActivity.kt
@@ -0,0 +1,12 @@
+package com.edouardondo.moneyklap.controller.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.edouardondo.moneyklap.R
+
+class RateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rate)
+    }
+}
